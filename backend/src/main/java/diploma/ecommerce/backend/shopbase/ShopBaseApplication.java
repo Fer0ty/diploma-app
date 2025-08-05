de84e97f3978b32f3d9af07388a0ff8e0bc6278b
Артемий <66246727+Fer0ty@users.noreply.github.com>
2025-08-05 12:25:13 +0300

    Update and rename test.txt to ShopBaseApplication.java

---
+++ backend/src/main/java/diploma/ecommerce/backend/shopbase/ShopBaseApplication.java
@@ -0,0 +1,13 @@
+package diploma.ecommerce.backend.shopbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBaseApplication.class, args);
+    }
+
+}
